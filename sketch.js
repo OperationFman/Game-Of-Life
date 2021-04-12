98d12c3d367a59c7f1fac8bf42c5386f2dc41a3e
Franklin Moon <franklin.m.moon@gmail.com>
2021-04-12 13:48:56 +1000

    Added p5 elements to show the grid based off resolution

--- sketch.js
+++ sketch.js
@@ -1,5 +1,7 @@
-let columns = 10;
-let rows = 10;
+let grid;
+let cols;
+let rows;
+let resolution = 40;
 
 function make2DArray(cols, rows) {
     let arr = new Array(cols);
@@ -9,13 +11,30 @@ function make2DArray(cols, rows) {
     return arr;
 }
 
-let grid;
-
 function setup() {
-    grid = make2DArray(columns, rows);
-    for (let i = 0; i < columns; i++) {
+    createCanvas(400, 400);
+    cols = width / resolution;
+    rows = height / resolution;
+
+    grid = make2DArray(cols, rows);
+    for (let i = 0; i < cols; i++) {
         for (let j = 0; j < rows; j++) {
             grid[i][j] = floor(random(2))
+        }
+    }
+}
+
+function draw() {
+    background(0);
+    for (let i = 0; i < cols; i++) {
+        for (let j = 0; j < rows; j++) {
+            let x = i * resolution;
+            let y = j * resolution;
+            
+            if (grid[i][j] == 1) {
+                fill(255);
+                rect(x, y, resolution, resolution);
+            }
         }
     }
 }
