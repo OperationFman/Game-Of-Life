62b7f955bde34251806d87df68c8bac180e0fdea
Franklin Moon <franklin.m.moon@gmail.com>
2021-04-12 13:31:46 +1000

    Complete refactor from boilerplate to official p5 docs

--- sketch.js
+++ sketch.js
@@ -6,16 +6,16 @@ function make2DArray(cols, rows) {
     for (let i = 0; i < arr.length; i++) {
         arr[i] = new Array(rows);
     }
+    return arr;
 }
 
 let grid;
 
 function setup() {
     grid = make2DArray(columns, rows);
-    for (let i = 0; i < cols; i++) {
+    for (let i = 0; i < columns; i++) {
         for (let j = 0; j < rows; j++) {
             grid[i][j] = floor(random(2))
         }
     }
-
 }
